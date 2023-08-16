213f9f631d2d8a98881106a206069f3633865965
kimlong <kimlong5244@gmail.com>
2023-08-16 12:36:47 +0700

    new icon

--- src/components/LinkBox.js
+++ src/components/LinkBox.js
@@ -1,32 +1,49 @@
 import React from "react";
+import { FaGlobe,FaPhone,FaGithub,FaTelegram ,FaTwitter, FaFacebook, FaInstagram, FaLinkedin, FaPinterest, FaSnapchat, FaYoutube, FaReddit, FaTumblr, FaTiktok } from 'react-icons/fa';
+import { SiGoogle } from 'react-icons/si';
+import { MdMailOutline } from 'react-icons/md';
+import { RiCloseCircleLine } from 'react-icons/ri'; 
+const IconSelector = ({ title }) => {
+  const lowerCaseTitle = title.toLowerCase();
+  const iconMapping = {
+    twitter: FaTwitter,
+    facebook: FaFacebook,
+    instagram: FaInstagram,
+    website: FaGlobe,
+    youtube: FaYoutube,
+    linkedin: FaLinkedin,
+    pinterest: FaPinterest,
+    snapchat: FaSnapchat,
+    reddit: FaReddit,
+    tumblr: FaTumblr,
+    tiktok: FaTiktok,
+    github: FaGithub,
+    telegram: FaTelegram,
+    phone: FaPhone,
+    google: SiGoogle,
+    mail: MdMailOutline,
+    x: RiCloseCircleLine,
+    // Add more mappings as needed
+  };
+
+  const IconComponent = iconMapping[lowerCaseTitle] || FaGlobe;
+
+  return <IconComponent size={32} />;
+};
 
 const LinkBox = ({ title, url }) => {
+ 
   return (
     <div>
       <a href={url}>
         <div className="my-2 flex h-16 items-center justify-between rounded-lg border-2 border-b-4 border-l-4 border-blue-300 px-4 shadow-xl">
           <div className="flex items-center">
-            {/* <img
-              alt="photo1"
-              className="w-10 rounded-full"
-              src="https://icon-library.com/images/2018/2298785_oreos-oreo-cookie-adult-costume-hd-png-download.png"
-            /> */}
+            {/* icon */}
+          <IconSelector title={title}/>
             <div className="ml-5">
               <div className="text-md font-semibold">{title}</div>
-              {/* <div className="flex mt-1 text-xs font-light text-gray-600">
-               <div className="line-clamp-1 w-full">{url}</div>
-                <svg
-                  className="h-3 w-3"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
-                  <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
-                </svg>
-              </div> */}
             </div>
-          </div>
+          </div>  
           <div></div>
         </div>
       </a>
